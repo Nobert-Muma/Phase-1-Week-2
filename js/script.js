c45cd9e5b1e4a6ca0bbb39c320414dec0ad064eb
Nobert-Muma <mumanobert17@gmail.com>
2024-07-08 17:53:50 +0300

    Added comments to the JavaScript file

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ addButton.addEventListener('click', function(e){
     e.preventDefault();
     let inputItem=inputValue.value;
     if (inputItem===""){
+        // To notify the user when they click 'Add' before adding an item into the text input field
         alert("Please Input an Item");
     } else {
         shoppingListItems.push(inputItem);
@@ -29,20 +30,24 @@ addButton.addEventListener('click', function(e){
         deleteButton.addEventListener('click', function(e){
             if (e.target.classList.contains("deleteBtn")){
                 const itemToDelete=e.target.parentElement;
+                // Removes an element from our document
                 itemToDelete.remove();
             }
         })    
         li.appendChild(checkButton);
         ol.appendChild(li);
+        // Sets the text input field free for another item to be added 
         inputValue.value="";
-        
     }
 })
+
 clearButton.addEventListener('click', function(){
+    // Removes all the li items 
     ol.innerHTML='';
 })
 
 ol.addEventListener("click", function(e){
+    // Checks for the element with the class name 'itemCheck' and changes the background color of its parent element to green
     if (e.target.classList.contains("itemCheck")){
         e.target.parentElement.style.backgroundColor="green";
     }
